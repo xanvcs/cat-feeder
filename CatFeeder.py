415451d196648764c7890c7ec9176eda761bf6bc
xanvcs <96662758+xanvcs@users.noreply.github.com>
2022-05-03 09:27:51 -0600

    final finished code

--- CatFeeder.py
+++ CatFeeder.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
-#run these commands to update Pi to latest libraries
-#sudo apt-get update
-#sudo apt-get upgrade
-#sudo apt-get install rpi.gpio
+# run these commands to update Pi to latest libraries
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install rpi.gpio
 
 from GmailWrapper import GmailWrapper
 
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
+
 HOSTNAME = 'imap.gmail.com'
 USERNAME = 'petfeeder569@gmail.com'
 PASSWORD = 'Screwdesign1!'
 
+
 def feedByGmail():
     gmailWrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
     ids = gmailWrapper.getIdsBySubject('feed cats')
-    if(len(ids) > 0):
+    if len(ids) > 0:
         try:
             feed()
             gmailWrapper.markAsRead(ids)
         except:
             print("Failed to feed pets, they're starvinggggg")
+
 
 def feed():
     # let the GPIO library know where we've connected our servo to the Pi
@@ -34,15 +38,18 @@ def feed():
 
         # spin left, right, then left again rather than in a continuous circle
         # to prevent the food from jamming the servo
-        for index in range(0, 3):
-            dutyCycle = 2.5 if (index % 2 == 0) else 12.5
-            servo.ChangeDutyCycle(dutyCycle)
-            # adjust the sleep time to have the servo spin longer or shorter in that direction
-            time.sleep(0.8)
+        servo.ChangeDutyCycle(2.5)
+        time.sleep(10)
+        servo.ChangeDutyCycle(12.5)
+        time.sleep(2)
+        servo.ChangeDutyCycle(2.5)
+        time.sleep(7)
+        
     finally:
-            # always cleanup after ourselves
-            servo.stop()
-            GPIO.cleanup()
+        # always cleanup after ourselves
+        servo.stop()
+        GPIO.cleanup()
+
 
 if __name__ == '__main__':
     # kick off the feeding process (move the servo)
